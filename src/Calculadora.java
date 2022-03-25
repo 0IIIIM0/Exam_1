a4d91fd7d52c2d417059ba0e17d9bb380a9d2ea5
0IIIIM0 <ignac23med@gmail.com>
2022-03-25 11:08:47 -0600

    creacion de getters and setters

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,6 +4,35 @@ public class Calculadora {
    private double primerValor;
    private double segundoValor;
 
+   public String getMarca() {
+      return marca;
+   }
 
+   public void setMarca(String marca) {
+      this.marca = marca;
+   }
 
+   public boolean isEsCientifica() {
+      return esCientifica;
+   }
+
+   public void setEsCientifica(boolean esCientifica) {
+      this.esCientifica = esCientifica;
+   }
+
+   public double getPrimerValor() {
+      return primerValor;
+   }
+
+   public void setPrimerValor(double primerValor) {
+      this.primerValor = primerValor;
+   }
+
+   public double getSegundoValor() {
+      return segundoValor;
+   }
+
+   public void setSegundoValor(double segundoValor) {
+      this.segundoValor = segundoValor;
+   }
 }
